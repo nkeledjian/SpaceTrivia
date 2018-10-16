b372f2e7c6793bebbcc8c2df7c8349f997a0cd45
nVar <nkeledjian@gmail.com>
2018-10-16 15:28:35 -0700

    Cleaned up app.js file - moved older code and notes off file

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,6 @@ var corGCount = $("#corGuessed");
 var incorGCount = $("#incorGuessed");
 var question = $("#q-and-a");
 
-
     var questions = [
         {
         question: "In what year did Neil Armstrong step foot on the moon?",
@@ -33,7 +32,8 @@ var question = $("#q-and-a");
         // set start button to hidden when clicked
         // Start button will initiate question q-and-a sequence
 
-        // div id=q-and-a - populate with question and answers
+        // set Start button to populate page with question and answers
+        // display q's and a's in: div id=q-and-a 
     };
 
     var timerRunning = false;
@@ -86,100 +86,4 @@ var question = $("#q-and-a");
             return minutes + ":" + seconds;
         },
     }
-    
 
-    //
-    // --Q-and-A method - multiple choice and true/false--
-    // store each question in string variable? or in array?
-
-    // Potential solution - use objects:
-    // Perhaps each question could be its own object
-    // then assign answers for each question object as a key
-
-    /*
-    let qOne = {
-        prompt: "In what year did Neil Armstrong step foot on the moon?", 
-        answer1: "1969",
-        answer2: "1968",
-        answer3: "1973",
-        answer4: "1965",
-        // display answers
-        // Click function to handle selected answer
-
-        // if user clicks on answer with a value that matches a given correct key for the answer - count as correct answer; update the corGuess var
-
-        // else incorrect answer; update incorGuess var 
-    };
-    let qTwo = {
-        prompt: "True or False: The Earth is 93 million miles away from the Sun", 
-        
-        answer: true,
-        // Click function to handle click for true/false
-    };
-    // End-game - display number of corGuess and incorGuess
-
-    function endGame() {
-        corGCount.text(corGuess);
-        incorGCount.text(incorGuess);
-      }
-      */
-
-
-
-      
-    // store all game functions within window.onload?  
-    // window.onload = function() {
-    //     // start timer
-    //     $("#start").on("click", timer.start); 
-    //     // -- Next to build --- 
-    //     // set start button to hidden when clicked
-    //     // Start button will initiate question q-and-a sequence
-
-    //     // div id=q-and-a - populate with question and answers
-    // };
-
-    
-
-    // prevents the clock from being sped up unnecessarily
-    // var timerRunning = false;
-
-    // // ---COUNTDOWN TIMER START---
-    // var timer = {
-
-    // time: 60,
-    // start: function() {
-    //     // setInverval will repeat timer.count every 1 second
-    //     if (!timerRunning) {
-    //     setInterval(timer.count, 1000 * 1);
-    //     timerRunning = true;
-    //     }
-    // },
-    // count: function() {
-
-    //     // decrement time by 1
-    //     timer.time--;
-
-    //     /* Get the current time, pass that into the timer.timeConverter function */
-    //     // result saved in a variable.
-    //     var converted = timer.timeConverter(timer.time);
-    //     console.log(converted);
-
-    //     // Display variable in "display" div.
-    //     $("#display").text(converted);
-    // },
-    // timeConverter: function(t) {
-    //     var minutes = Math.floor(t / 60);
-    //     var seconds = t + (minutes * 60);
-
-    //     if (seconds < 1) {
-    //     seconds = "0" + seconds;
-    //     }
-    //     if (minutes === 0) {
-    //     minutes = "00";
-    //     }
-    //     else if (minutes < 10) {
-    //     minutes = "0" - minutes;
-    //     }
-    //     return minutes + ":" + seconds;
-    // }
-//   };// ---COUNTDOWN TIMER END---
