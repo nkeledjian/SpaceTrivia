1daf9a7ae49cde93fc7f97e41577ccd8d206410c
nVar <nkeledjian@gmail.com>
2018-10-17 21:35:35 -0700

    Updates to app.js, html page and styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,13 +53,14 @@ var question = $("#q-and-a");
 
         /* implement for loop on for loop to access the space object and it's arrays of questions create a whole separate function to handle this? Should this be part of the game.start object?*/
         // for (var i=0; i<space.length; i++) {
+        //     // logic
         //     console.log(space.length);
         //     console.log(space[i].question);
             
-        //     for(var j=0; j<space[i].answer.length; i++) {
-
+        //     for(var j=0; j<space[j].answer.length; j++) {
+        //         // if user selects correct radio button = corGuess++
         //         console.log(space[i].answer)
-        //         // if user clicks corect space.answer, corGuess++
+        //         // 
         //         // else is user clicks incorrect space.answer, incorGuess++
         //     }
         // }
@@ -75,9 +76,9 @@ var question = $("#q-and-a");
     // Radio buttons for form - assign value to <input> tags
         // If user selects correct <input> = corGuess++
 
-    console.log(space[0]);
-    console.log(space[1]);
-    console.log(space[2]);
+    console.log("Moon question", space[0].answer);
+    console.log("Sun question", space[1].correctAnswer);
+    console.log("Element question", space[2]);
     var timerRunning = false;
     var game = {
         corGuess: 0, // Correct guesses
@@ -87,11 +88,17 @@ var question = $("#q-and-a");
         countDown: function() {
             // decrement time by 1
             game.time--;
+            if(game.time === 0){
+                console.log('end the game');
+                game.endGame();
+            }
+            console.log('keep running');
+            
 
             /* Get the current time, pass that into the game.timeConverter function */
             // result saved in var "converted"
             var converted = game.timeConverter(game.time);
-            console.log(converted);
+            // console.log(converted);
 
             // Display variable in "display" div.
             $("#display").text(converted);
@@ -103,18 +110,21 @@ var question = $("#q-and-a");
             // load();
         },
         load: function(){
-            // call start function
+            // game.start - start function called
             // display one question using current question
             // for loop for accessing answers only
-            for(var j=0; j<space[i].answer.length; i++) {
-                
-                
-                // if user clicks corect space.answer, corGuess++
-                // else is user clicks incorrect space.answer, incorGuess++
+            for (var i=0; i<space[i].length; i++) {
+                console.log("space[i]", space[i]);
+                for(var j=0; j<space[j].answer[j].length; j++) {
+                    console.log("Space answer", space[j].answer);
+                    
+                    // if user clicks corect space.answer, corGuess++
+                    // else is user clicks incorrect space.answer, incorGuess++
+                }
             }
         },
-
         start: function() {
+            console.log('start called');
             if (!timerRunning) {
                 // setInverval will decrement 1 every 1 second
                 setInterval(game.countDown, 1000 * 1);
@@ -125,13 +135,17 @@ var question = $("#q-and-a");
                 alert("Out of Time! Game Over!");
                 
             }
-            console.log('start func running...');
+            game.load();
+            
 
         },
-        /* Considering adding score clearing instructions for the endGame function */
+        
         endGame: function() {
-            corGCount.text(corGuess);
-            incorGCount.text(incorGuess);
+            /* Considering adding score clearing instructions for the endGame function */
+            console.log("Scores Updated and GAME OVER");
+            // UPDATE SCORE
+            // corGCount.text(corGuess);
+            // incorGCount.text(incorGuess);
         },
         // This converts game.time to look like a digital timer - still needs a bit of tweaking to work with more than 60 seconds of time
         timeConverter: function(t) {
